f8309b100e1f1f7a38d3de2a545473fdeef06002
3li <ali.abdelaal2054@gmail.com>
2021-07-26 23:09:02 +0200

    Update yUiSELF.bat

--- yUiSELF.bat
+++ yUiSELF.bat
@@ -1,4 +1,4 @@
 cd /d "C:\Users\ali20\anaconda3\Scripts\"
 call C:\Users\ali20\anaconda3\Scripts\activate.bat
 call activate tensorflow
-python C:\Users\ali20\Downloads\yUicHess\src\yui_chess\run.py --cmd self --type normal
+python C:\Users\ali20\Downloads\yUicHess\src\yui_chess\run.py --cmd self --type config
