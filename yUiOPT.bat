6d3a3220512cac1674d5e94060f36fac727bce44
3li <ali.abdelaal2054@gmail.com>
2021-07-26 23:14:02 +0200

    done

--- yUiOPT.bat
+++ yUiOPT.bat
@@ -1,4 +1,4 @@
 cd /d "C:\Users\ali20\anaconda3\Scripts\"
 call C:\Users\ali20\anaconda3\Scripts\activate.bat
 call activate tensorflow
-python C:\Users\ali20\Downloads\yUicHess\src\yui_chess\run.py --cmd opt --type normal --total-step 50
+python C:\Users\ali20\Downloads\yUicHess\src\yui_chess\run.py --cmd opt --type options --total-step 50
